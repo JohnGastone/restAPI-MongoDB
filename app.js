c6a902d6e03210d1c1e4896f446f9469454559ec
JohnGastone <johngastone11@gmail.com>
2023-03-22 17:34:26 +0300

    Added express module

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("I am running Nodejs and I use nodemon to re-run changes automatically")
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        if (req.method === "GET") {
+            console.log('It is a GET request')
+        } else if (req.method === 'POST') {
+            console.log('It is a POST request')
+        } else {
+            console.log('It is just another request')
+        }
+        res.write('You are at the homepage ')
+    res.end();
+    } else if (req.url === ('/another')) {
+        res.write('I am from another route ')
+    res.end();
+    }
+    else{
+    res.write('I am listening to your requests ')
+    res.end();}
+})
+
+server.listen(3000, () => {
+    console.log('Our server will be at port 3000')
+})
