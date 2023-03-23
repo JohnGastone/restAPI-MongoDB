080930296899ca7f1da178b425d2be275b21a7a3
JohnGastone <johngastone11@gmail.com>
2023-03-23 09:35:38 +0300

    POST & GET requests success

--- routes/products.route.js
+++ routes/products.route.js
@@ -5,4 +5,8 @@ router.get('/', (req, res, next) => {
     res.send('A response from express router')
 })
 
+router.post('/', (req, res, next) => {
+    res.send('Some products created')
+})
+
 module.exports = router;
