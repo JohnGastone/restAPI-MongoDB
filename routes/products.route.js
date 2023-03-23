227414fb035136317a146e5b204a258eaafd1dbe
JohnGastone <johngastone11@gmail.com>
2023-03-23 09:47:05 +0300

    Added PATCH and DELETE functionalities

--- routes/products.route.js
+++ routes/products.route.js
@@ -9,4 +9,15 @@ router.post('/', (req, res, next) => {
     res.send('Some products created')
 })
 
+router.get('/:id', (req, res, next) => {
+    res.send('Get a specific product by its id')
+})
+
+router.patch('/:id', (req, res, next) => {
+    res.send('Update a specific product by its id')
+})
+
+router.delete('/:id', (req, res, next) => {
+    res.send('Delete a specific product by its id')
+})
 module.exports = router;
