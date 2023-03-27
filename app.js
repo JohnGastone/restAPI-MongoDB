8ceb431c29c9eeb62fad1167041681192289e799
JohnGastone <johngastone11@gmail.com>
2023-03-27 10:45:40 +0300

    ++

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/RestAPI_Products').then(() => {
+mongoose.connect('mongodb://localhost:27017/RestAPI_Products', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.log('Mongodb connected...')
 })
 
