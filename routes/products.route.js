1f702c7b302439073ffaa04967c32a305948f002
JohnGastone <johngastone11@gmail.com>
2023-03-30 08:41:24 +0300

    Querying products

--- routes/products.route.js
+++ routes/products.route.js
@@ -3,10 +3,18 @@ const router = express.Router()
 
 const Product = require('../models/product.model')
 
-router.get('/', (req, res, next) => {
-    //next(new Error('Can not get a list of all products'))
-    res.send('A response from express router')
+// Get a list of all products 
+router.get('/', async (req, res, next) => {    
+    //res.send('Getting a list of all products')
+    try {
+        const results = await Product.find({price: 550}, {name: 1, _id : 0, price : 1})
+        res.send(results)
+    } catch (error)
+    {
+        console.log(error.message)    
+    }
 })
+
 
 router.post('/', async (req, res, next) => {
     // Saving a product using async & wait
@@ -37,14 +45,17 @@ router.post('/', async (req, res, next) => {
     res.send('Some products created')
 })
 
+// Get a product by its id
 router.get('/:id', (req, res, next) => {
     res.send('Get a specific product by its id')
 })
 
+// Update a product by its id
 router.patch('/:id', (req, res, next) => {
     res.send('Update a specific product by its id')
 })
 
+// Delete a product by its id
 router.delete('/:id', (req, res, next) => {
     res.send('Delete a specific product by its id')
 })
